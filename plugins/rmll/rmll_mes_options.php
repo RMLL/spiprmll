ad194d330172b0480c17372b18ff160e12b6e604
Odile Bénassy <obenassy@free.fr>
2013-05-09 22:30:41 +0200

    Fichier de paramètres.

--- plugins/rmll/rmll_mes_options.php
+++ plugins/rmll/rmll_mes_options.php
@@ -1,7 +1,7 @@
 <?php
-    define('RMLL_PROG_ID', '37');
-    define('RMLL_THEME_ID', '43,44,45,47');
-    define('RMLL_SESSION_ID', '12,39,40,7,41,42,15,16,17,18,19,20,33,14,10,11,9,13,46,54,55,56,57,58,59');
+    define('RMLL_PROG_ID', '37'); # FIXME
+    define('RMLL_THEME_ID', '43,44,45,47'); # FIXME
+    define('RMLL_SESSION_ID', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,27');
     define('RMLL_KEYWORDS_GROUP_ID', 1);
 
     function rmll_is_prog($id) {
@@ -20,23 +20,42 @@
     define('RMLL_PERIOD_CONF', 1);
     define('RMLL_PERIOD_PAUSE', 2);
     define('RMLL_PERIOD_LUNCH', 3);
+    define('RMLL_PERIOD_CONF_PE', 4);
 
     $rmll_schedule_periods = array();
     $rmll_schedule_periods['default'] = array(
         array('start' => '09:20', 'end' => '10:40', 'type' => RMLL_PERIOD_CONF),
         array('start' => '10:40', 'end' => '11:00', 'type' => RMLL_PERIOD_PAUSE),
-        array('start' => '11:00', 'end' => '12:40', 'type' => RMLL_PERIOD_CONF),
-        array('start' => '12:40', 'end' => '14:00', 'type' => RMLL_PERIOD_LUNCH),
+        array('start' => '11:00', 'end' => '12:20', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '12:20', 'end' => '14:00', 'type' => RMLL_PERIOD_LUNCH),
         array('start' => '14:00', 'end' => '16:00', 'type' => RMLL_PERIOD_CONF),
         array('start' => '16:00', 'end' => '16:20', 'type' => RMLL_PERIOD_PAUSE),
         array('start' => '16:20', 'end' => '18:00', 'type' => RMLL_PERIOD_CONF),
         array('start' => '18:00', 'end' => '20:00', 'type' => RMLL_PERIOD_PAUSE),
         array('start' => '20:00', 'end' => '23:59', 'type' => RMLL_PERIOD_CONF),
     );
-    $rmll_schedule_periods['2012-07-07'] = array(
-        array('start' => '10:00', 'end' => '18:00', 'type' => RMLL_PERIOD_CONF),
+    $rmll_schedule_periods['2013-07-09'] = array(
+        array('start' => '09:20', 'end' => '10:40', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '10:40', 'end' => '11:00', 'type' => RMLL_PERIOD_PAUSE),
+        array('start' => '11:00', 'end' => '12:20', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '12:20', 'end' => '15:00', 'type' => RMLL_PERIOD_LUNCH),
+        array('start' => '15:00', 'end' => '18:00', 'type' => RMLL_PERIOD_CONF_PE),
+        array('start' => '18:00', 'end' => '19:00', 'type' => RMLL_PERIOD_PAUSE),
+        array('start' => '19:00', 'end' => '23:59', 'type' => RMLL_PERIOD_CONF),
     );
-    $rmll_schedule_periods['2012-07-08'] = $rmll_schedule_periods['2012-07-07'];
+    $rmll_schedule_periods['2013-07-10'] = array(
+        array('start' => '10:00', 'end' => '12:20', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '12:20', 'end' => '14:00', 'type' => RMLL_PERIOD_LUNCH),
+        array('start' => '14:00', 'end' => '16:00', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '16:00', 'end' => '16:20', 'type' => RMLL_PERIOD_PAUSE),
+        array('start' => '16:20', 'end' => '18:00', 'type' => RMLL_PERIOD_CONF),
+        array('start' => '18:00', 'end' => '20:00', 'type' => RMLL_PERIOD_PAUSE),
+        array('start' => '20:00', 'end' => '23:59', 'type' => RMLL_PERIOD_CONF),
+    );
+    /*$rmll_schedule_periods['2012-07-07'] = array(
+        array('start' => '10:00', 'end' => '18:00', 'type' => RMLL_PERIOD_CONF),
+    ); */
+    //$rmll_schedule_periods['2012-07-08'] = $rmll_schedule_periods['2012-07-07'];
 
     //$rmll_schedule_periods['theme_33'] = $rmll_schedule_periods['2012-07-07'];
 
